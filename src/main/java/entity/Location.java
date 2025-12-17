cd3022ef66368ad6e88bf9027880af558585aa96
bala-amypo <bala@amypo.in>
2025-12-17 05:16:16 +0000

    User commit

--- src/main/java/entity/Location.java
+++ src/main/java/entity/Location.java
@@ -1,1 +1,13 @@
-package
+package com.example.demo.entity;
+import jakarta.persistence.*;
+
+@Entity
+public class Location{
+    @Id
+    private Long id;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+
+    
+}
