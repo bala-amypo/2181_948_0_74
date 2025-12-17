f86abc91ccc344b9acc0c62671519c297013e9ed
bala-amypo <bala@amypo.in>
2025-12-17 05:01:16 +0000

    User commit

--- src/main/java/entity/Vehicle.java
+++ src/main/java/entity/Vehicle.java
@@ -11,5 +11,45 @@ private Long id;
 @ManyToOne
 private User user;
 
-private 
+@Column(unique=true)
+private String vechicleNumber;
+
+private Double capacityKg;
+private Double fuelEfficiency;
+
+public Long getId(){
+    return id;
+}
+public void setId(Long id){
+    this.id=id;
+}
+
+public User getUser(){
+    return user;
+}
+public void setUser(User user){
+    this.user=user;
+}
+
+public String getVehicleNumber(){
+    return id;
+}
+public void setId(Long id){
+    this.id=id;
+}
+
+public Long getId(){
+    return id;
+}
+public void setId(Long id){
+    this.id=id;
+}
+
+public Long getId(){
+    return id;
+}
+public void setId(Long id){
+    this.id=id;
+}
+
 }
